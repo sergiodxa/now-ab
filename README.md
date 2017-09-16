3871d16fa9af3ee8d6dd8893e32d137758664f34
Sergio Daniel Xalambrí <sergiodxa@gmail.com>
2017-09-16 21:29:55 +0200

    Proxy AB server

---
+++ README.md
@@ -0,0 +1,15 @@
+# now-ab
+This a simple simple proxy server to handle [Now.sh](https://now.sh) deployments AB tests.
+
+## Usage
+Deploy it with
+
+```shell
+now sergiodxa/now-ab
+```
+
+Now will prompt you to define 2 environment variables `TEST_A` and `TEST_B` with urls (including the protocol).
+
+When an user access this server is going to proxy the request to one of those (doing a simple random to define which one) and will set a cookie called `now_ab` with the user test case (`TEST_A` or `TEST_B`).
+
+You can also add more test variables to support more test cases just adding `-e TEST_C=$URL` when deploying, the server will use any variable starting with `TEST_` to get possible test cases.
---
+++ index.js
@@ -0,0 +1,80 @@
+require('now-env').config();
+
+const { parse, format } = require('url');
+const { createServer } = require('http');
+const { createProxy } = require('http-proxy');
+
+const HOST = process.env.HOST || 'localhost';
+const PORT = Number(process.env.PORT) || 3000;
+
+// get the list of tes cases urls
+const testCases = Object.entries(process.env)
+  .filter(([key]) => key.startsWith('TEST_'))
+  .reduce((acc, [key, value]) => {
+    acc[key] = value;
+    return acc;
+  }, {});
+
+// create a proxy
+const proxy = createProxy({
+  changeOrigin: true,
+  target: {
+    https: true
+  }
+});
+
+// parse cookies from headers
+const parseCookies = req => {
+  const list = {};
+  const rc = req.headers.cookie;
+
+  rc &&
+    rc.split(';').forEach(cookie => {
+      const parts = cookie.split('=');
+      list[parts.shift().trim()] = decodeURI(parts.join('='));
+    });
+
+  return list;
+};
+
+// get the test URL to proxy
+const getTestURL = (res, cookies = {}) => {
+  const cases = Object.keys(testCases);
+  if (!cookies.now_ab) {
+    const random = Math.floor(Math.random() * cases.length);
+    const testCase = cases[random];
+    // set the cookie now_ab with the random
+    // (don't let the user know the internal url)
+    res.setHeader('Set-Cookie', [`now_ab=${testCase}`]);
+    return testCases[testCase];
+  }
+  return testCases[cookies.now_ab];
+};
+
+// run the HTTP Proxy server
+createServer((req, res) => {
+  const cookies = parseCookies(req);
+  const url = parse(req.url);
+
+  // get the url host from cookies if it's possible or random
+  url.host = getTestURL(res, cookies);
+
+  // if it's not defined anymore (we removed the test) just get it again random
+  if (url.host === undefined) {
+    url.host = getTestURL(res);
+  }
+
+  console.log(`Proxying to ${url.host}`);
+
+  // format the url host
+  const target = format(url);
+
+  // proxy to the specific target
+  return proxy.web(req, res, { target });
+}).listen(PORT, error => {
+  if (error) {
+    console.error(error);
+    return process.exit(0);
+  }
+  console.log('A/B Test Server running on %s:%d', HOST, PORT);
+});
